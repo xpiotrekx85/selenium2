7f12d24bb656e8f3a71adb2234a73c8e2542fbe5
Piotrek <34603318+xpiotrekx85@users.noreply.github.com>
2024-10-19 15:46:24 +0200

    initial commit

---
+++ src/test/java/CartTests.java
@@ -0,0 +1,79 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.jupiter.api.*;
+
+
+public class CartTests {
+    WebDriver driver;
+    String calculusSlug = "/calculus-made-easy-by-silvanus-p-thompson/";
+    String historyOfAstronomySlug = "/history-of-astronomy-by-george-forbes/";
+
+    @BeforeEach
+    public void setup() {
+        driver = new FirefoxDriver();
+    }
+    @AfterEach
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void no_product_added_to_cart_should_cart_be_empty() {
+        CartPage cartPage = new CartPage(driver);
+        cartPage.go();
+
+        Assertions.assertEquals(0, cartPage.getNumberOfProducts(),
+                "Number of products in cart is not 0.");
+    }
+    @Test
+    public void product_added_to_cart_should_cart_have_one_product() {
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = productPage.go(calculusSlug).addToCart().goToCart();
+        int numberOfProducts = cartPage.getNumberOfProducts();
+
+        Assertions.assertEquals(1, numberOfProducts,
+                "Expected number of products in cart: 1" +
+                        "\nActual: " + numberOfProducts);
+    }
+
+    @Test
+    public void two_products_added_to_cart_should_cart_have_two_products() {
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = productPage
+                .go(calculusSlug).addToCart()
+                .go(historyOfAstronomySlug).addToCart().goToCart();
+
+        int numberOfProducts = cartPage.getNumberOfProducts();
+        Assertions.assertEquals(2, numberOfProducts,
+                "Expected number of products in cart: 2" +
+                        "\nActual: " + numberOfProducts);
+    }
+
+    @Test
+    public void changing_quantity_in_cart_should_change_total_price() {
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = productPage
+                .go(calculusSlug)
+                .addToCart()
+                .goToCart()
+                .changeQuantity(3);
+
+        Assertions.assertEquals("39,00 €",
+                cartPage.getTotalPrice(),
+                "Total price after quantity update is not what expected.");
+    }
+
+    @Test
+    public void changing_quantity_in_cart_to_negative_should_not_update_total_price() {
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = productPage
+                .go(calculusSlug)
+                .addToCart()
+                .goToCart()
+                .changeQuantity(-3);
+
+        Assertions.assertEquals("13,00 €",
+                cartPage.getTotalPrice(),
+                "Total price after quantity update is not what expected.");
+    }
+}
